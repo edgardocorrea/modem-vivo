cabf35ed179b526039ef9d1169113d78a1c2229f
Edgardo Correa <edgardo.correa@hotmail.com>
2025-11-12 13:11:25 -0300

    Add files via upload

--- desinstalar.bat
+++ desinstalar.bat
@@ -1,3 +1,9 @@
 @echo off
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0desinstalar.ps1"
+REM Este arquivo apenas inicia o script de desinstalação com privilégios de administrador.
+REM A lógica de copiar e executar de outro lugar está dentro do script .ps1
+
+powershell.exe -ExecutionPolicy Bypass -File "desinstalar.ps1"
+
+REM Se o script pedir para reiniciar, o .bat fecha e o novo processo tomará conta.
+REM Se falhar, a janela ficará aberta para o usuário ver o erro.
 pause
